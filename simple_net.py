Fixed temperature scaling in cross entropy interpolation

simple_net.py
@@ -24,9 +24,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # RUN PARAMETERS
-Ts = np.logspace(0,np.log10(60),8)  # range should be fine, increase intervals when ready
+Ts = np.logspace(0,np.log10(30),5)  # range should be fine, increase intervals when ready
 alphas = np.linspace(0,1,5)         # range IS fine, increase intervals when ready
-num_repeat = 5                      # could be 10
+num_repeat = 10                     # could be 10
 batch_size = 50                     # no need to change I guess
 num_epochs = 2                      # could be changed... 5? 10? 20? write_logits.py uses 20.
 hidden_sizes = [800, 800]           # could be increased when ready...
@@ -77,7 +77,7 @@ for T in Ts:
 
             cross_entropy_soft = tf.reduce_mean(-tf.reduce_sum(y_soft_ * tf.log(y_out_soft), reduction_indices=[1]))
             cross_entropy_hard = tf.reduce_mean(-tf.reduce_sum(y_hard_ * tf.log(y_out_hard), reduction_indices=[1]))
-            cross_entropy = (alpha*cross_entropy_soft) + ((1-alpha)*cross_entropy_hard)
+            cross_entropy = ((T**2)*alpha*cross_entropy_soft) + ((1-alpha)*cross_entropy_hard)
             train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
             correct_prediction = tf.equal(tf.argmax(y_out_hard,1), tf.argmax(y_hard_,1))
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))