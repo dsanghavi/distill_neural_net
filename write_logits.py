40276258c1fbcc4605f7737118c24ee106b0f982
sudhamsu <sudongit@gmail.com>
2016-11-23 19:25:39 -0500

    Update parameters

--- write_logits.py
+++ write_logits.py
@@ -25,59 +25,59 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 sess = tf.InteractiveSession()
 
-# x = tf.placeholder(tf.float32, shape=[None, 784])
-# y_ = tf.placeholder(tf.float32, shape=[None, 10])
-#
-# W_conv1 = weight_variable([5, 5, 1, 32])
-# b_conv1 = bias_variable([32])
-#
-# x_image = tf.reshape(x, [-1,28,28,1])
-#
-# h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-# h_pool1 = max_pool_2x2(h_conv1)
-#
-# W_conv2 = weight_variable([5, 5, 32, 64])
-# b_conv2 = bias_variable([64])
-#
-# h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-# h_pool2 = max_pool_2x2(h_conv2)
-#
-# W_fc1 = weight_variable([7 * 7 * 64, 1024])
-# b_fc1 = bias_variable([1024])
-#
-# h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-# h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-#
-# keep_prob = tf.placeholder(tf.float32)
-# h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-#
-# W_fc2 = weight_variable([1024, 10])
-# b_fc2 = bias_variable([10])
-
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+
+x_image = tf.reshape(x, [-1,28,28,1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
 keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-#Layers
-W1 = weight_variable([28 * 28, hidden_sizes[0]])
-b1 = bias_variable([hidden_sizes[0]])
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
 
-# x_image = tf.reshape(x, [-1,28*28]) # maybe as sanity check
-h1 = tf.nn.relu(tf.matmul(x, W1) + b1)
-
-h1_dropped = tf.nn.dropout(h1, keep_prob)
-
-W2 = weight_variable([hidden_sizes[0], hidden_sizes[1]])
-b2 = bias_variable([hidden_sizes[1]])
-h2 = tf.nn.relu(tf.matmul(h1_dropped, W2) + b2)
-
-h2_dropped = tf.nn.dropout(h2, keep_prob)
-
-W3 = weight_variable([hidden_sizes[1], 10])
-b3 = bias_variable([10])
-y_conv = tf.matmul(h2_dropped, W3) + b3
-
-# y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+#
+# x = tf.placeholder(tf.float32, shape=[None, 784])
+# y_ = tf.placeholder(tf.float32, shape=[None, 10])
+# keep_prob = tf.placeholder(tf.float32)
+#
+# #Layers
+# W1 = weight_variable([28 * 28, hidden_sizes[0]])
+# b1 = bias_variable([hidden_sizes[0]])
+#
+# # x_image = tf.reshape(x, [-1,28*28]) # maybe as sanity check
+# h1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+#
+# h1_dropped = tf.nn.dropout(h1, keep_prob)
+#
+# W2 = weight_variable([hidden_sizes[0], hidden_sizes[1]])
+# b2 = bias_variable([hidden_sizes[1]])
+# h2 = tf.nn.relu(tf.matmul(h1_dropped, W2) + b2)
+#
+# h2_dropped = tf.nn.dropout(h2, keep_prob)
+#
+# W3 = weight_variable([hidden_sizes[1], 10])
+# b3 = bias_variable([10])
+# y_conv = tf.matmul(h2_dropped, W3) + b3
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
@@ -106,5 +106,6 @@ for i in range(iters_per_epoch):
 
 y_conv_np = np.concatenate(y_conv_nps,axis=0) # new shape = (55000, 10)
 
-np.savetxt('logits_mnist2.txt', y_conv_np)
+np.savetxt('logits_mnist1.txt', y_conv_np)
+# np.savetxt('logits_mnist2.txt', y_conv_np)
 # to read, y_read = np.loadtxt('<filename.txt>')
