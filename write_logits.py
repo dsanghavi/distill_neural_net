207db9b21464f56bac66c3906bfc36dbfb46c962
dsanghavi <darpansanghavi1993@gmail.com>
2016-12-06 00:40:21 -0500

    Fully Structured, mostly final script for finetuning as well as writing to file in same run, using validation data to tune, and report ultimate results on test set.

--- write_logits.py
+++ write_logits.py
@@ -10,7 +10,6 @@ hidden_sizes = [1200, 1200] # NOT USING THIS.
 # CURRENT DEFAULT HIDDEN SIZES = [1024] followed by output layer of size [10]
 
 def weight_variable(shape):
-  print "SHAPE - ", shape
   initial = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(initial)
 
@@ -61,13 +60,10 @@ def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu, op
             preactivate = operation(input_tensor, weights) + biases
             tf.histogram_summary(layer_name + '/pre_activations', preactivate)
         # Non linearity
-        if act is not None:
-            activations = act(preactivate, name='activation')
-            tf.histogram_summary(layer_name + '/activations', activations)
+        activations = act(preactivate, name='activation')
+        tf.histogram_summary(layer_name + '/activations', activations)
 
-        if act is not None:
-            return activations
-        return preactivate
+        return activations
 
 def launch_graph_and_train_with_summaries(lr=1e-4,kp=0.2,batch_size=50,num_epochs=20,sess=None):
     if sess is None:
@@ -96,35 +92,59 @@ def launch_graph_and_train_with_summaries(lr=1e-4,kp=0.2,batch_size=50,num_epoch
     h_fc1 = nn_layer(h_pool2_flat,7*7*64,1024,'fc1',act=tf.nn.relu,operation=tf.matmul) 
 
     # Dropout
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    with tf.name_scope('dropout'):
+        keep_prob = tf.placeholder(tf.float32)
+        tf.scalar_summary('dropout_keep_probability', keep_prob)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-    # FC1 Layer
-    y_conv = nn_layer(h_fc1_drop,1024,10,'fc2',act=tf.nn.relu,operation=tf.matmul) 
-
+    # FC2 Layer
+    y_conv = nn_layer(h_fc1_drop,1024,10,'fc2',act=tf.identity,operation=tf.matmul) 
+    
     # TRAINING
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-    train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    with tf.name_scope('cross_entropy'):
+        diff = tf.nn.softmax_cross_entropy_with_logits(y_conv, y_)
+        with tf.name_scope('total'):
+            cross_entropy = tf.reduce_mean(diff)
+        tf.scalar_summary('cross entropy', cross_entropy)
+
+    with tf.name_scope('train'):
+        train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+    with tf.name_scope('accuracy'):
+        with tf.name_scope('correct_prediction'):
+            correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+        with tf.name_scope('accuracy'):
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.scalar_summary('accuracy', accuracy)
+
+    # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
+    merged = tf.merge_all_summaries()
+    train_writer = tf.train.SummaryWriter('tensorboard_logs/mnist_logs' + '/train',
+                                        sess.graph)
+    # Test writer actually logs validation accuracies
+    val_writer = tf.train.SummaryWriter('tensorboard_logs/mnist_logs' + '/val')
 
     sess.run(tf.initialize_all_variables())
 
-    ############CROSS VALIDATION!?!?!?!????????????? TECHNICALLY, SHOULDN'T USE THE TEST DATA FOR HP. IS THAT CHEATING 
-    ################################################ IN THIS CASE THOUGH, WHERE WE ONLY CARE ABOUT THE TRANSFER OF KNOWLEDGE?
-
     iters_per_epoch = mnist.train.num_examples/batch_size
     for i in range(num_epochs*iters_per_epoch):
-      batch = mnist.train.next_batch(batch_size,shuffle=False)
-      if i%5000 == 0:
-          train_accuracy = accuracy.eval(feed_dict={
-                  x:batch[0], y_: batch[1], keep_prob: 1.0})
-          print("step %d, training accuracy %g"%(i, train_accuracy))
-      train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: kp})
+        batch = mnist.train.next_batch(batch_size,shuffle=False)
+        # Log test stats
+        if i%50 == 0:
+            summary, val_accuracy = sess.run([merged,accuracy],feed_dict={x:mnist.validation.images, y_: mnist.validation.labels, keep_prob: 1.0}) 
+            val_writer.add_summary(summary, i)
+            print("step %d, intermediate val accuracy %g"%(i, val_accuracy))
 
-    print("lr: %g, keep_prob: %g, test accuracy %g"%(lr,kp,accuracy_in_batches(mnist.validation, accuracy, x, y_, keep_prob, batch_size=batch_size)))
-    # print("lr: %g, keep_prob: %g, test accuracy %g"%(lr,kp,accuracy_in_batches(mnist.test, accuracy, x, y_, keep_prob, batch_size=batch_size)))
-    return x, y_conv, keep_prob
+        summary, _ = sess.run([merged, train_step],feed_dict={x: batch[0], y_: batch[1], keep_prob: kp})
+        train_writer.add_summary(summary, i)
+
+    train_writer.close()
+    val_writer.close()
+
+    val_acc = accuracy_in_batches(mnist.validation, accuracy, x, y_, keep_prob, batch_size=batch_size)
+    print("lr: %g, keep_prob: %g, val_accuracy: %g"%(lr,kp,val_acc))
+    
+    return x, y_, y_conv, keep_prob, accuracy, val_acc
 
 def launch_graph_and_train(lr=1e-4,kp=0.2,batch_size=50,num_epochs=20,sess=None):
     if sess is None:
@@ -171,99 +191,60 @@ def launch_graph_and_train(lr=1e-4,kp=0.2,batch_size=50,num_epochs=20,sess=None)
 
     sess.run(tf.initialize_all_variables())
 
-    ############CROSS VALIDATION!?!?!?!????????????? TECHNICALLY, SHOULDN'T USE THE TEST DATA FOR HP. IS THAT CHEATING 
-    ################################################ IN THIS CASE THOUGH, WHERE WE ONLY CARE ABOUT THE TRANSFER OF KNOWLEDGE?
-
     iters_per_epoch = mnist.train.num_examples/batch_size
     for i in range(num_epochs*iters_per_epoch):
       batch = mnist.train.next_batch(batch_size,shuffle=False)
-      if i%5000 == 0:
-          train_accuracy = accuracy.eval(feed_dict={
-                  x:batch[0], y_: batch[1], keep_prob: 1.0})
-          print("step %d, training accuracy %g"%(i, train_accuracy))
+      # if i%50 == 0:
+      #     train_accuracy = accuracy.eval(feed_dict={
+      #             x:batch[0], y_: batch[1], keep_prob: 1.0})
+      #     print("step %d, training accuracy %g"%(i, train_accuracy))
       train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: kp})
 
-    print("lr: %g, keep_prob: %g, test accuracy %g"%(lr,kp,accuracy_in_batches(mnist.validation, accuracy, x, y_, keep_prob, batch_size=batch_size)))
-    # print("lr: %g, keep_prob: %g, test accuracy %g"%(lr,kp,accuracy_in_batches(mnist.test, accuracy, x, y_, keep_prob, batch_size=batch_size)))
-    return x, y_conv, keep_prob
+    val_acc = accuracy_in_batches(mnist.validation, accuracy, x, y_, keep_prob, batch_size=batch_size)
+    print("lr: %g, keep_prob: %g, val_accuracy %g"%(lr,kp,val_acc))
+    
+    return x, y_, y_conv, keep_prob, accuracy, val_acc
 
 def fine_tune():
+    best_val_acc = None
+    best_lr = None
+    best_kp = None
+
+    batch_size = 50
+    num_epochs = 20 
+    iters_per_epoch = mnist.train.num_examples/batch_size
+
     # lrs = np.logspace(-6,-2,7) # rough space
     # lrs = np.logspace(-5,-3,7)
     lrs = np.logspace(-5,-2,15)[3:-1]
     # keep_probs = np.arange(5)/5.0
     keep_probs = [0.1,0.2,0.3,0.4,0.5]
-    # status = 0
+
     for lr in lrs:
-        # status += 1
         for kp in keep_probs:
             tf.reset_default_graph()
-
             sess = tf.InteractiveSession()
+            _, _, _, _, _, val_acc = launch_graph_and_train(lr=lr,kp=kp,batch_size=batch_size,num_epochs=num_epochs,sess=sess)
 
-            x = tf.placeholder(tf.float32, shape=[None, 784])
-            y_ = tf.placeholder(tf.float32, shape=[None, 10])
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                best_lr = lr
+                best_kp = kp
 
-            W_conv1 = weight_variable([5, 5, 1, 32])
-            b_conv1 = bias_variable([32])
-
-            x_image = tf.reshape(x, [-1,28,28,1])
-
-            h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-            h_pool1 = max_pool_2x2(h_conv1)
-
-            W_conv2 = weight_variable([5, 5, 32, 64])
-            b_conv2 = bias_variable([64])
-
-            h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-            h_pool2 = max_pool_2x2(h_conv2)
-
-            W_fc1 = weight_variable([7 * 7 * 64, 1024])
-            b_fc1 = bias_variable([1024])
-
-            h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-            keep_prob = tf.placeholder(tf.float32)
-            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-            W_fc2 = weight_variable([1024, 10])
-            b_fc2 = bias_variable([10])
-
-            y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-
-            cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-            train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
-            correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            sess.run(tf.initialize_all_variables())
-
-            batch_size = 50
-            num_epochs = 20
-            iters_per_epoch = mnist.train.num_examples/batch_size
-            for i in range(num_epochs*iters_per_epoch):
-                batch = mnist.train.next_batch(batch_size,shuffle=False)
-                # if i%5000 == 0:
-                #     train_accuracy = accuracy.eval(feed_dict={
-                #             x:batch[0], y_: batch[1], keep_prob: 1.0})
-                #     print("step %d, training accuracy %g"%(i, train_accuracy))
-                train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: kp})
-
-            print("lr: %g, keep_prob: %g, test accuracy %g"%(lr,kp,accuracy_in_batches(mnist.test, accuracy, x, y_, keep_prob, batch_size=batch_size)))
-            # print "STATUS ----> ", status, "/15"
             sess.close()
+
+    return best_lr, best_kp
     
-def write_best_logits(sess=None):
+def write_best_logits(sess=None, lr=0.000517947, kp=0.2):
     # # lr = 0.00019307 # 1.93e-4
     # lr = 0.000848343 # 8.48e-4
-    lr = 0.000517947 # 5.18e-4
-    kp = 0.2
+    # lr = 0.000517947 # 5.18e-4
+    # kp = 0.2
     batch_size = 50
     num_epochs = 20 
     iters_per_epoch = mnist.train.num_examples/batch_size
 
-    x, y_conv, keep_prob = launch_graph_and_train_with_summaries(lr=lr,kp=kp,batch_size=batch_size,num_epochs=num_epochs,sess=sess)
-
+    x, y_, y_conv, keep_prob, accuracy_tf, _ = launch_graph_and_train_with_summaries(lr=lr,kp=kp,batch_size=batch_size,num_epochs=num_epochs,sess=sess)
 
     # WRITE THE LOGITS AFTER TUNING-----------------------------  
     # write out logits before softmax
@@ -274,14 +255,17 @@ def write_best_logits(sess=None):
 
     y_conv_np_all = np.concatenate(y_conv_nps,axis=0) # new shape = (55000, 10)
 
-    np.savetxt('logits_mnist_tuned3_52errors.csv', y_conv_np_all, delimiter = ', ')
+    np.savetxt('logits_mnist_tuned4.csv', y_conv_np_all, delimiter = ', ')
     # to read, y_read = np.loadtxt('<filename.txt>',delimiter=', ')
 
+    test_accuracy = accuracy_in_batches(mnist.test, accuracy_tf, x, y_, batch_size=batch_size)
+    print '\n\nTEST ACCURACY =', test_accuracy
+
 # Uncomment for fine tuning ---------------------------------------------------
-# fine_tune()
+best_lr, best_kp = fine_tune()
 
 # AFTER FINE-TUNING, WRITE THE BEST LOGITS ------------------------------------
 tf.reset_default_graph()
 sess = tf.InteractiveSession()
-write_best_logits(sess=sess)
+write_best_logits(sess=sess, lr=best_lr, kp=best_kp)
 sess.close()
